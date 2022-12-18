95e8d02aea4cb46fa6bb011cef3f186a7ae7c1eb
Twashi1 <thomas.zarri@gmail.com>
2022-12-18 14:53:34 +0000

    comments

--- SparseSetECS/Core.h
+++ SparseSetECS/Core.h
@@ -5,20 +5,20 @@
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
-#include <type_traits>
 #include <tuple>
+#include <type_traits>
 #include <vector>
 
 #include "Logger.h"
 
-#define ECS_ID_TYPE   std::uint32_t
-#define ECS_SIZE_TYPE std::uint32_t
+#define ECS_ID_TYPE		std::uint32_t
+#define ECS_SIZE_TYPE	std::uint32_t
 #define ECS_SPARSE_PAGE 4096U
-#define ECS_PACKED_PAGE 1024U
-#define ECS_ENTITY_MAX  0xfffffU
-#define ECS_VERSION_MAX 0xfffU
+#define ECS_PACKED_PAGE 1024U	 // TODO: unused (packed arrays aren't paged)
+#define ECS_ENTITY_MAX  0xfffffU // 5 * 4 bits (20)
+#define ECS_VERSION_MAX 0xfffU   // 3 * 4 bits (12)
 
-#define ECS_MAX_COMPONENTS 64U
+#define ECS_MAX_COMPONENTS 64U   // Arbitrary value for maximum amount of components (can be changed)
 
 #define ECS_ENTITY_BIT_SIZE		20U
 #define ECS_VERSION_SHIFT_ALIGN 20U // Should be same value as above
